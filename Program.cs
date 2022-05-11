17491c68fdf545ecc463651a81e8969def40a34f
jenotsuns <100307281+jenotsuns@users.noreply.github.com>
2022-05-11 21:21:11 +0300

    Added changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,61 +9,34 @@ namespace Minesweeper
 {
     internal class Program
     {
-       struct CellStruct
+        static void Main(string[] args)
         {
-            public bool hasFlag;
-            public bool hasMine;
-            public bool isUncovered;
-            public int neighbourBombs;
+            Mines mines = new Mines();
+            mines.MinePlanter();
+            Program program = new Program();
+            program.CoordinateInput();
         }
-        const int boardSize = 10;
-        CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
-        bool gameOver;
- 
-        public void CountNeighbours()
-        {
-            for (int row = 0; row < boardSize; row++)
-            {
-                for (int column = 0; column < boardSize; column++)
-                {
-                    CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
-                    int mineCount = 0; // why can't i declare it as <int mineCount;> ?
-                    if (cell[row - 1, column - 1].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row - 1, column].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row - 1, column + 1].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row, column - 1].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row, column + 1].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row + 1, column - 1].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row + 1, column].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    if (cell[row + 1, column + 1].hasMine)
-                    {
-                        mineCount++;
-                    }
-                    cell[row, column].neighbourBombs = mineCount;
-                }
-            }
-        }
+        /* public override void MinePlanter() // ??
+         {
+             for (int row = 0; row < boardSize; row++)
+             {
+                 for (int column = 0; column < boardSize; column++)
+                 {
+                     if (cell[row, column].hasFlag)
+                     {
+                         Console.Write("F");
+                     }
+                     else if (cell[row, column].isUncovered)
+                     {
+                         Console.Write("#");
+                     }
+                     else
+                     {
+                        // Console.Write(mineCount); // not working
+                     }
+                 }
+             }
+         } */
         public void CoordinateInput()
         {
             Console.WriteLine($"\nEnter the coordinates (letter + number): ");
@@ -82,17 +55,5 @@ namespace Minesweeper
             }
         }
     }
-    public class PrintInConsole
-    {
-        static void Main(string[] args)
-        {
 
-            // CountNeighbours(); // not needed i guess
-            Mines mines = new Mines();
-            mines.MinePlanter();
-            Program program = new Program();
-            program.CoordinateInput();
-
-        }
-    }
 }
