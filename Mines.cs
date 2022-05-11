daa9593be4c7a4371e1425c44b1393f567381e11
jenotsuns <100307281+jenotsuns@users.noreply.github.com>
2022-05-11 16:01:43 +0300

    Added files

---
+++ Mines.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+    internal class Mines
+    {
+        struct CellStruct
+        {
+            public bool hasFlag;
+            public bool hasMine;
+            public bool isUncovered;
+            public int neighbourBombs;
+        }
+        const int boardSize = 10;
+        CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
+        bool gameOver;
+        public virtual void MinePlanter() // virtual so that it could be inherited from
+        {
+            //Program E = new Program();
+            //E.CellStruct();
+            //CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
+            Random rand = new Random();
+            bool[] n = new bool[100];
+            for (int x = 0; x < 90; x++)
+            {
+                n[x] = false;
+            }
+            for (int x = 90; x < 100; x++)
+            {
+                n[x] = true;
+            }
+            for (int x = 0; x < 100; x++)
+            {
+                int pos = rand.Next(100);
+                bool save = n[x];
+                n[x] = n[pos];
+                n[pos] = save;
+            }
+            for (int x = 0; x < 100; x++)
+            {
+                int column = x % 10;
+                int row = x / 10;
+                cell[row, column].hasMine = n[x];
+            }
+            for (int x = 0; x < 100; x++)
+            {
+                if (n[x])
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write("#"); //■
+                }
+                if ((x + 1) % 10 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+    class Draw : Mines
+    {
+        struct CellStruct // copied as it's inaccessible due to its protection level 
+        {
+            public bool hasFlag;
+            public bool hasMine;
+            public bool isUncovered;
+            public int neighbourBombs;
+        }
+        const int boardSize = 10;
+        CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
+        bool gameOver;
+        public virtual void MinePlanter()
+        {
+            //const int boardSize = 10; // 'Mines.boardSize' is inaccessible due to its protection level 
+            //  CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2]; /
+            
+            for (int row = 0; row <boardSize; row++)
+            {
+                for (int column = 0; column < boardSize; column++)
+                {
+                    if (cell[row, column].hasFlag)
+                    {
+                        Console.Write("F");
+                    }
+                    else if (cell[row, column].isUncovered)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(mineCount);
+                    }
+                }
+            }
+        }
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Minesweeper
+{
+    internal class Program
+    {
+       struct CellStruct
+        {
+            public bool hasFlag;
+            public bool hasMine;
+            public bool isUncovered;
+            public int neighbourBombs;
+        }
+        const int boardSize = 10;
+        CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
+        bool gameOver;
+ 
+        public void CountNeighbours()
+        {
+            for (int row = 0; row < boardSize; row++)
+            {
+                for (int column = 0; column < boardSize; column++)
+                {
+                    CellStruct[,] cell = new CellStruct[boardSize + 2, boardSize + 2];
+                    int mineCount = 0; // why can't i declare it as <int mineCount;> ?
+                    if (cell[row - 1, column - 1].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row - 1, column].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row - 1, column + 1].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row, column - 1].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row, column + 1].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row + 1, column - 1].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row + 1, column].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    if (cell[row + 1, column + 1].hasMine)
+                    {
+                        mineCount++;
+                    }
+                    cell[row, column].neighbourBombs = mineCount;
+                }
+            }
+        }
+        public void CoordinateInput()
+        {
+            Console.WriteLine($"\nEnter the coordinates (letter + number): ");
+            string playerInput = Console.ReadLine();
+            Console.WriteLine($"You entered: {playerInput}");
+        }
+        static bool again()
+        {
+            while (true)
+            {
+                Console.WriteLine("Would you like to play again? (Y/N)");
+                char answer = Convert.ToChar(Console.ReadLine());
+                if (answer == 'Y' || answer == 'y') return true;
+                if (answer == 'N' || answer == 'n') return false;
+                // + needs to repeat the printing loop
+            }
+        }
+    }
+    public class PrintInConsole
+    {
+        static void Main(string[] args)
+        {
+
+            // CountNeighbours(); // not needed i guess
+            Mines mines = new Mines();
+            mines.MinePlanter();
+            Program program = new Program();
+            program.CoordinateInput();
+
+        }
+    }
+}
