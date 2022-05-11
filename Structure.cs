587e96260e45cfc1e2dc9420791f0f9f1d94c3f0
jenotsuns <100307281+jenotsuns@users.noreply.github.com>
2022-05-11 21:22:15 +0300

    Add files via upload

---
+++ Structure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper
+{
+      public class CellStruct
+        {
+            public bool hasFlag;
+            public bool hasMine;
+            public bool isUncovered;
+            public int neighbourBombs;
+        }
+}
